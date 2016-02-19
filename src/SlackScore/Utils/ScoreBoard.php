d618e1fc8302bcbd4378943db40a6f1797c1dbc8
Johan Johansson <johan@bazookian.net>
2016-02-19 11:40:40 +0100

    Sort user score list by score desc

--- src/SlackScore/Utils/ScoreBoard.php
+++ src/SlackScore/Utils/ScoreBoard.php
@@ -5,7 +5,7 @@
     class ScoreBoard {
         public static function render($users) {
             $output = '';
-            foreach($users as $user) {
+            foreach(usort($users, function($a, $b) { return $a->score == $b->score ? 0 : ($a->score < $b->score ? 1 : -1); }) as $user) {
                 $output .= '@' . $user->name . ' = ' . $user->score . "\n";
             }
             
